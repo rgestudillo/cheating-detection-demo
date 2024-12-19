3732ac051195e87d563aafdf30098967de8dccf8
rgestudillo <rgestudillo@up.edu.ph>
2024-12-19 22:08:05 +0800

    modified to use env

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
     // Log the body to the console
     console.log('Request Body:', JSON.stringify(requestBody, null, 2));
 
-    const response = await fetch('http://127.0.0.1:8000/analyze', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/analyze`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
